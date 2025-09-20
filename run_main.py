84154601b9048a51bf2800f12347c41cd1a14ec1
Florian Barthel <florianbarthel@yahoo.de>
2025-09-20 09:58:17 +0200

    remove unused modes

--- run_main.py
+++ run_main.py
@@ -4,12 +4,11 @@ from splatviz import Splatviz
 
 @click.command()
 @click.option("--data_path", help="root path for .ply files", metavar="PATH", default="./resources/sample_scenes")
-@click.option("--mode", help="[default, decoder, attach, gan, ffhq]", default="default")
+@click.option("--mode", help="[default, attach]", default="default")
 @click.option("--host", help="host address", default="127.0.0.1")
 @click.option("--port", help="port", default=6007)
-@click.option("--gan_path", help="path to Gaussian GAN Decoder project", default="", type=click.Path())
-def main(data_path, mode, host, port, gan_path):
-    splatviz = Splatviz(data_path=data_path, mode=mode, host=host, port=port, gan_path=gan_path)
+def main(data_path, mode, host, port):
+    splatviz = Splatviz(data_path=data_path, mode=mode, host=host, port=port)
     while not splatviz.should_close():
         splatviz.draw_frame()
     splatviz.close()
